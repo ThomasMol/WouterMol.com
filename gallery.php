830746da28b765359473f692def4502a75043a0c
Thomas Mol <email@thomasmol.com>
2016-08-23 00:06:07 +0200

    Navbar
    Created Navbar, added a theme and gallery page.

---
+++ gallery.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="http://use.edgefonts.net/basic:n4:all.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="theme-color" content="#fff" />
+    <link rel="shortcut icon" type="image/png" href="img/logo.ico" />    
+    <link rel="stylesheet" href="https://bootswatch.com/sandstone/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>WouterMol.com | Gallery</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">                
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#id-collapse-top-nav" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>                
+                <a href="#" class="navbar-brand">WouterMol.com</a>
+            </div>            
+            <div class="collapse navbar-collapse navbar-right" id="id-collapse-top-nav">      
+                <ul class="nav navbar-nav">
+                    <!-- change in production!! -->
+                    <li><a href="../WouterMol">home</a></li>
+                    <li class="active"><a href="gallery">gallery</a></li>
+                </ul>
+            </div>
+        </div>        
+    </nav>
+
+    <section id="main">
+    </section>
+
+    <footer>
+    </footer>
+    
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+<script src="script.js"></script>
+</body>
+</html>
