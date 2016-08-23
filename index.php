e1e66e2a8c03b0633a9ce6b244ea54cdfbdc671b
Thomas Mol <email@thomasmol.com>
2016-08-23 18:08:33 +0200

    Index.php and gallery.php
    Added image viewer using php.
    Loads navbar with images sorted in categories.
    Loads images in said category.

--- index.php
+++ index.php
@@ -2,20 +2,22 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <base href="//192.168.2.86/projects/WouterMol/"/>
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="http://use.edgefonts.net/basic:n4:all.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
     <meta name="mobile-web-app-capable" content="yes" />
-    <meta name="theme-color" content="#fff" />
+    <meta name="theme-color" content="#000" />
     <link rel="shortcut icon" type="image/png" href="img/logo.ico" />    
-    <link rel="stylesheet" href="https://bootswatch.com/sandstone/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
     <title>WouterMol.com | Home</title>
 </head>
 
 <body>
 
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">                
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#id-collapse-top-nav" aria-expanded="false">
@@ -29,19 +31,25 @@
             <div class="collapse navbar-collapse navbar-right" id="id-collapse-top-nav">      
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="home">home</a></li>
-                    <li><a href="gallery">gallery</a></li>
+                    <li><a href="gallery/All">gallery</a></li>
                 </ul>
             </div>
         </div>        
     </nav>
 
     <section id="main">
+    <div class="jumbotron index">
+        <div class="container">
+            <h1 id="hello-message">Wouter Mol</h1>
+            <h3 id="sub-message">photography</h3>
+        </div>
+    </div>
+        
+    
     </section>
 
     <footer>
     </footer>
     
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
-<script src="script.js"></script>
 </body>
 </html>
