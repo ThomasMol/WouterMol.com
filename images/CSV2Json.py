35a21140518bd2d8629638751da2b5438204874b
Wouter <wouterbmol@gmail.com>
2016-08-26 18:07:47 +0200

    parse the .csv file and generate a final database.json file based off of it

---
+++ images/CSV2Json.py
@@ -0,0 +1,33 @@
+import json, os
+
+def convert(fname):
+	csv = open(fname,'r').read().split('\n')
+	database = []
+	
+	header = True
+	for line in csv[:-1]:
+		if header:
+			keys = line.split(';')
+			header = False
+		else:
+			contents = line.split(';')
+			item = {}
+			for i in range(len(keys)):
+				if ', ' in contents[i]:
+					item[keys[i]] = contents[i].split(', ')
+				else:
+					item[keys[i]] = contents[i]
+			database.append(item)
+	
+	if os.path.exists('database/database.json'):
+		print 'Warning! database already exists, saving backup to databaseOLD.json'
+		os.rename('database/database.json','database/databaseOLD.json')
+	
+	with open('database/database.json','w') as f:
+		json.dump(database, f)
+	
+				
+		
+
+if __name__ == '__main__':
+	convert('database/init.csv')
