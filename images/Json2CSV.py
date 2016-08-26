b4645eada5e1414285a0c2bc4417fd4685718a10
Wouter <wouterbmol@gmail.com>
2016-08-26 18:07:27 +0200

    order attributes differently

--- images/Json2CSV.py
+++ images/Json2CSV.py
@@ -1,17 +1,26 @@
-import json
-
+import json, os
 
 def convert(fname):
 	with open(fname,'r') as f:
 		database = json.load(f)
+	
+	if os.path.exists('database/init.csv'):
+		print 'warning! init.csv already exists, backup to initOLD.csv'
+		os.rename('database/init.csv','database/initOLD.csv')
 		
-	csv = open('database.csv','w')
+	csv = open('database/init.csv','w')
+	order = ['filename','name','categories','albums',
+			 'description','location','date','time','thumbnail','timestamp']
+	order_ix = {}
 	
 	header = True
-	
 	for entry in database:
 		if header:
-			names = ';'.join(entry.keys())
+			names = entry.keys()
+			for name in names:
+				order_ix[names.index(name)] = order.index(name)
+			names.sort(key= lambda x: order.index(x))
+			names = ';'.join(names)
 			header = False
 			csv.write('%s\n'%names)
 		
@@ -19,11 +28,13 @@ def convert(fname):
 		for i in range(len(contents)):
 			if type(contents[i]) == list:
 				contents[i] = ', '.join(contents[i])
-		#print contents
+		contents = sorted(contents,key= lambda x: order_ix[contents.index(x)])
 		csv.write('%s\n'%(';'.join(contents)))
 	
 	csv.close()
-
-convert('database.json')
+	
+if __name__ == '__main__':
+	convert('database/init.json')
+	
 		
 
