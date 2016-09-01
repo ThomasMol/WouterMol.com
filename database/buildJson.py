b0678b105a61e2423fd74e1b85e2a2df30d384e5
wouter <wouterbmol@gmail.com>
2016-09-01 17:46:31 +0200

    add location key, change indentation to spaces, catch EXIF no key exception

--- database/buildJson.py
+++ database/buildJson.py
@@ -11,53 +11,57 @@ from time import mktime
 force = True #force rewrite database json
 
 def reformatDtime(dtime):
-	dt = datetime.strptime(str(dtime), '%Y:%m:%d %H:%M:%S')
-	unix = str(int(mktime(dt.timetuple())*1000))
-	return dt.strftime('%Y/%m/%d'), dt.strftime('%H:%M'), unix
+    dt = datetime.strptime(str(dtime), '%Y:%m:%d %H:%M:%S')
+    unix = str(int(mktime(dt.timetuple())*1000))
+    return dt.strftime('%Y/%m/%d'), dt.strftime('%H:%M'), unix
 
 def extractExif(fpath):
-	""" extract some exif data of the image located at filepath """
-		
-	with open(fpath, 'r') as f:
-		exif = exifread.process_file(f)
-		
-	date, time, timestamp = reformatDtime(exif['EXIF DateTimeOriginal'])
-	baseName, extension = fpath.split('/')[-1][:-4], fpath.split('.')[-1]
-	return {'date':date,
-			'time':time,
-			'timestamp':timestamp,
-			'categories':[],
-			'name':'',
-			'description':'',
-			'filename': baseName+'.'+extension,
-			'albums':[],
-			'thumbnail':baseName+'_thumb.'+extension}
+    """ extract some exif data of the image located at filepath """
+        
+    with open(fpath, 'r') as f:
+        exif = exifread.process_file(f)
+    
+    try:
+        date, time, timestamp = reformatDtime(exif['EXIF DateTimeOriginal'])
+    except Exception, e:
+        date, time, timestamp = None
+    baseName, extension = fpath.split('/')[-1][:-4], fpath.split('.')[-1]
+    return {'date':date,
+            'time':time,
+            'timestamp':timestamp,
+            'categories':[],
+            'name':'',
+            'description':'',
+            'filename': baseName+'.'+extension,
+            'albums':[],
+            'thumbnail':baseName+'_thumb.'+extension,
+            'location': ''}
 
 if __name__ == '__main__':
-	images = sorted(glob('../images/*.jpg'))
-	
-	if os.path.exists('database.json'):
-		try:
-			with open('database.json','r') as f:
-				database = json.load(f)
-		except Exception, e:
-			print 'ERROR loading database json'
-			print str(e)
-			if raw_input('continue to overwrite database json? type "yes": ').lower() == 'yes':
-				database = []
-			
-	else:
-		database = []
+    images = sorted(glob('../images/*.jpg'))
+    
+    if os.path.exists('database.json'):
+        try:
+            with open('database.json','r') as f:
+                database = json.load(f)
+        except Exception, e:
+            print 'ERROR loading database json'
+            print str(e)
+            if raw_input('continue to overwrite database json? type "yes": ').lower() == 'yes':
+                database = []
+            
+    else:
+        database = []
 
-	for image in images:
-		img_name = image.split('/')[-1][:-4]
-		if not img_name in database or force:
-			exif = extractExif(image)
-			database.append(exif)
-			print 'added',img_name,'record'
-		else:
-			print img_name,'already exists in database, skipping..'
-	
-	with open('database.json','w') as f:
-		json.dump(database, f)
-		
+    for image in images:
+        img_name = image.split('/')[-1][:-4]
+        if not img_name in database or force:
+            exif = extractExif(image)
+            database.append(exif)
+            print 'added',img_name,'record'
+        else:
+            print img_name,'already exists in database, skipping..'
+    
+    with open('database.json','w') as f:
+        json.dump(database, f)
+        
