e0388803f26f91b945d70bfef9cd1e6750f45d80
wouter <wouterbmol@gmail.com>
2016-09-02 21:39:26 +0200

    use fit method instead of resize and paste on 16:9 format

--- database/genThumbnails.py
+++ database/genThumbnails.py
@@ -1,18 +1,21 @@
-from PIL import Image
+from PIL import Image, ImageOps
 from glob import glob
 
 size = 256, 144
 images = glob('../images/*.jpg')
 images = [i for i in images if 'thumb' not in i]
 
-for image in images:
+def resize(image):
     print image
     name, ext = image[2:].split('.')
     expName = '..' + name+'_thumb.'+ext
     img = Image.open(image)
-    img.thumbnail(size, Image.ANTIALIAS)
-    background = Image.new('RGBA', size, (255, 255, 255, 0))
-    background.paste(img, ((size[0] - img.size[0]) / 2, (size[1] - img.size[1]) / 2))
-    background.save(expName, "JPEG", quality=95)
+    img = ImageOps.fit(img, size, Image.ANTIALIAS)
+    img.save(expName, "JPEG", quality=95)
     img.close()
-    background.close()
+    
+if __name__ == '__main__':
+    for image in images:
+        resize(image)
+     
+    
